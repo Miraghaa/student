c9743380e08c56388439a9f4e8a72b4a7a2574c6
Miraghaa <miraghaea@code.edu.az>
2023-04-09 21:51:34 +0400

    student

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,6 @@
+﻿studenta studenta = new studenta("Miraga", "Aliyev");
+studenta studenta1 = new studenta("Miraga", "Aliyev", "p234", 85);
+
+
+studenta.getfullname();
+studenta1.getfullinfo1();
---
+++ ConsoleApp4/ConsoleApp4/studenta.cs
@@ -0,0 +1,51 @@
+﻿class studenta
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public int Point;
+    public bool isgraduated;
+    int yearofstudy = 3;
+
+    public studenta(string name, string Surname, string group, int point)
+
+    {
+        this.Name = name;
+        this.Surname = Surname;
+        this.Group = group;
+        this.Point = point;
+        this.isgraduated = false;
+    }
+    public studenta(string name, string Surname)
+    {
+        this.Name = name;
+        this.Surname = Surname;
+    }
+
+    public void getfullname()
+    {
+
+        Console.WriteLine($"{this.Name} {this.Surname}");
+
+    }
+    public void getfullinfo1()
+    {
+        Console.WriteLine($"{this.Name} {this.Surname} {this.Group} {this.Point}");
+        if (yearofstudy > 4)
+        {
+            isgraduated = true;
+            Console.WriteLine("mezun olub oturub evde deyir is yoxdu");
+
+        }
+        Console.WriteLine("mezun olmuyuf hele oxuyur akademik olajax");
+        if (Point > 80)
+        {
+            Console.WriteLine("imtahan seni gozleyir.ugurlar");
+        }
+        else
+        {
+            Console.WriteLine("bextivi bir daha yoxla");
+        }
+
+    }
+}
